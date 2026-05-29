e7068354740c6cde69dfba0f6b3960a4d96651ba
RubenSalgado <123654353+RubenSalgadoDev@users.noreply.github.com>
2026-05-29 08:29:48 -0500

    Add files via upload

---
+++ frontend.js
@@ -0,0 +1,34 @@
+document.getElementById('formProveeedores').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData.entries());
+
+    const btn = document.getElementById('btnRegistrar');
+    btn.disabled = true;
+    btn.innerText = 'Procesando registro...';
+
+    try {
+        const responde = await fetch('/api/proveedores' ,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        const resultado = await responde.json();
+        if (response.ok){
+            alert('¡Proveedor registrado exitosamente!');
+            e.target.reset();
+        }else{
+            alert('Error en el registro: ' + resultado.error);
+        }
+    } catch (error) {
+        console.error('Error de comunicación:', error);
+        alert('No se pudo establecer conexión con el servidor.');
+    }finally{
+        btn.disabled=false;
+        btn.innerText = 'Registrar Proveedor';
+    }
+});
---
+++ server.js
@@ -0,0 +1,71 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import pg from 'pg';
+
+const { Client } = pg;
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json());
+app.use(express.static(__dirname));
+
+const dbConfig = {
+    user: 'proveedores_0cr9_user',
+    host: 'dpg-d87kui8js32c73ee8tf0-a.oregon-postgres.render.com',
+    database: 'proveedores_0cr9',
+    password: 'J4WjtV7R5c62kXb5aqvtUH3ZRuv3dMR',
+    port: 5432,
+    ssl: { rejectUnauthorized: false }
+};
+
+
+app.opst('/api/proveedores', async (req, res) => {
+    const client = new Client(dbConfig);
+
+    const {
+        nit, estado, razon_social, tipo_proveeodor,
+        fecha_creacion, ultima_actualizacion,
+        correo_rut, correo_comercial, correo_compras,
+        correo_ea, correo_pagos, correo_tributario
+    } = req.body;
+
+    try {
+        await client.connect();
+
+        const queryText = `INSERT INTO proveedores (
+            nit, estado, razon_social, tipo_proveeodor,
+            fecha_creacion, ultima_actualizacion,
+            correo_rut, correo_comercial, correo_compras,
+            correo_ea, correo_pagos, correo_tributario
+        ) VALUES ($1, $2, $3, $4 ,$5 ,$6 ,$7 ,$8 ,$9 ,$10 ,$11 ,$12)
+        RETURNING id;
+        `;
+
+        const values = [
+            nit, estado, razon_social, tipo_proveeodor,
+            fecha_creacion, ultima_actualizacion,
+            correo_rut, correo_comercial, correo_compras || null,
+            correo_ea || null, correo_pagos, correo_tributario
+        ];
+
+        const result = await clien.query(queryText, values);
+
+        res.status(200).json({ success: true, id: result.rows[0].id });
+    } catch (err) {
+        console.error('Error al insertar en la BD:', err.message);
+    } finally {
+        await client.end();
+    }
+});
+
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () =>{
+    console.log(`Servidor local corriendo de manera exitosa en el puerto ${PORT}`)
+});
